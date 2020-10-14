9f9ec8f8f527781690703c1cc87613c04fc6d1b7
Francisco Sicardi <fsicardir@gmail.com>
2020-10-14 18:10:36 -0300

    Add simple documentation

--- tp0/base64.h
+++ tp0/base64.h
@@ -3,5 +3,14 @@
 
 #define PADDING_CHAR '='
 
+/*
+ * Encodes the given array to base64, returning a NULL terminated string
+ * On error returns NULL.
+ */
 unsigned char *base64_encode(const unsigned char *input, size_t input_size);
+
+/*
+ * Decodes the given array from base64, returning a NULL terminated string
+ * Returns NULL if the given array is invalid.
+ */
 unsigned char *base64_decode(const unsigned char *input, size_t input_size);
